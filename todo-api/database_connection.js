c1d60cb0207b82ab7b6464b7893290771cd4e00e
rohit369 <scorpiorohit311@gmail.com>
2025-01-19 12:23:53 +0530

    Add pg-pool for better connection management

--- todo-api/database_connection.js
+++ todo-api/database_connection.js
@@ -1,11 +1,14 @@
-const { Client } = require('pg');
+const { Pool } = require('pg');
 
-const dbClient = new Client({
+const dbClient = new Pool({
 	user: 'postgres',
 	password: 'postgres',
 	host: 'localhost',
 	port: '5432',
 	database: 'todo_api',
+  max: 20, // maximum number of connections in the pool
+  idleTimeoutMillis: 30000, // how long a client is allowed to remain idle before being closed
+  connectionTimeoutMillis: 2000, // how long to wait for a connection to be established
 });
 
 
