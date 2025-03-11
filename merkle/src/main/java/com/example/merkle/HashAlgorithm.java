923ceb863c2c273bfd0329793b6774e67653eb0f
rohit369 <scorpiorohit311@gmail.com>
2025-03-11 18:08:16 +0530

    Merkle tree implementation

---
+++ merkle/src/main/java/com/example/merkle/HashAlgorithm.java
@@ -0,0 +1,16 @@
+package com.example.merkle;
+
+import java.nio.charset.StandardCharsets;
+
+import org.bouncycastle.jcajce.provider.digest.Keccak;
+import org.bouncycastle.util.encoders.Hex;
+
+public class HashAlgorithm {
+  public static String generateHash(String originalString) {
+    Keccak.Digest256 digest256 = new Keccak.Digest256();
+    byte[] hashedByteArray = digest256.digest(
+            originalString.getBytes(StandardCharsets.UTF_8));
+
+    return new String(Hex.encode(hashedByteArray));
+  }
+}
---
+++ merkle/src/test/java/com/example/merkle/MerkleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.merkle;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MerkleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
