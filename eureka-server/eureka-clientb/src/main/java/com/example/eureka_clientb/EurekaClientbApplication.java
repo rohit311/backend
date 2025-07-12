e4bcaadb6caf4cd2d6d67002340c25109051d58b
rohit369 <scorpiorohit311@gmail.com>
2025-07-12 10:47:14 +0530

    Eureka Client 2

---
+++ eureka-server/eureka-clientb/src/main/java/com/example/eureka_clientb/EurekaClientbApplication.java
@@ -0,0 +1,13 @@
+package com.example.eureka_clientb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EurekaClientbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EurekaClientbApplication.class, args);
+	}
+
+}
---
+++ eureka-server/eureka-clientb/src/test/java/com/example/eureka_clientb/EurekaClientbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.eureka_clientb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaClientbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
