4f7c8b19f18e8dcf1f3ca5ddee8f8d8626713b67
rohit369 <scorpiorohit311@gmail.com>
2025-07-12 17:10:26 +0530

    Hypermedia REST API
    Reference - https://spring.io/guides/gs/rest-hateoas

---
+++ hypermedia/src/main/java/com/example/hypermedia/HypermediaApplication.java
@@ -0,0 +1,13 @@
+package com.example.hypermedia;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HypermediaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HypermediaApplication.class, args);
+	}
+
+}
