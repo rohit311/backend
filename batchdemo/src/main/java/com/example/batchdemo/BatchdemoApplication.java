024a2c9059d3df39bc14e2cfa22e97856a5940de
rohit369 <scorpiorohit311@gmail.com>
2025-08-20 17:54:43 +0530

    Spring batch demo

---
+++ batchdemo/src/main/java/com/example/batchdemo/BatchdemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.batchdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BatchdemoApplication {
+
+	public static void main(String[] args) {
+		System.exit(SpringApplication.exit(SpringApplication.run(BatchdemoApplication.class, args)));
+	}
+
+}
---
+++ batchdemo/src/main/java/com/example/batchdemo/Person.java
@@ -0,0 +1,5 @@
+package com.example.batchdemo;
+
+public record Person(String firstName, String lastName) {
+
+}
