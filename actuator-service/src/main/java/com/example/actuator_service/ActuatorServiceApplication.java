9cf2ae94fac9764116a7e3eecc2703dba23377ee
rohit369 <scorpiorohit311@gmail.com>
2025-09-15 12:47:23 +0530

    Spring actuator

---
+++ actuator-service/src/main/java/com/example/actuator_service/ActuatorServiceApplication.java
@@ -0,0 +1,13 @@
+package com.example.actuator_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ActuatorServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ActuatorServiceApplication.class, args);
+	}
+
+}
