38eac72363a6a26e3723ab7e076dcbf2887daddd
rohit369 <scorpiorohit311@gmail.com>
2026-09-01 22:46:25 +0530

    Add UDPClient class for sending messages

---
+++ simple-udp/UDPClient.java
@@ -0,0 +1,30 @@
+import java.io.IOException;
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+import java.net.InetAddress;
+import java.util.Scanner;
+
+public class UDPClient {
+    
+    public static void main(String[] args) throws IOException {
+        DatagramPacket sendPacket;
+        byte[] sendData;
+
+        DatagramSocket clientSocket = new DatagramSocket();
+        clientSocket.setSoTimeout(1000);
+        Scanner input = new Scanner(System.in);
+
+        while (true) {
+            String cmd = input.nextLine();
+            // If client types quit, close the socket and exit
+            if (cmd.equals("QUIT")) {
+                clientSocket.close();
+                input.close();
+                System.exit(1);
+            }
+            sendData = cmd.getBytes();
+            sendPacket = new DatagramPacket(sendData, sendData.length, InetAddress.getByName("127.0.0.1"), 5001);
+            clientSocket.send(sendPacket);
+        }
+    }
+}
